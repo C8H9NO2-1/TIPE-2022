4e78c7faabb05ce4e852606ac8d647488135b79f
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2022-05-22 17:41:31 +0200

    Changing things

--- main.py
+++ main.py
@@ -9,34 +9,36 @@ def g(z):
 	return G*MT/(z+6400e3)**2
 
 p0=1e5 #Pa
-z=np.linspace(0,1500000,1500000) #altitude
+z=np.linspace(0,15000000,15000000) #altitude
 
-##Température en fonction de l'altitude
-def Temp (z):
-	if z < 11e3 : return -6.5*z*1e-3 + 15
-	elif z < 20e3 : return -56.5
-	elif z < 32e3 : return 1*(z - 20e3)*1e-3 - 56.5
-	elif z < 47e3 : return 2.8*(z - 32e3)*1e-3 - 44.5
-	elif z < 51e3 : return -2.5
-	elif z < 71e3 : return -2.8*(z - 51e3)*1e-3 - 2.5
-	else : return -2*(z - 71e3)*1e-3 - 58.5
+##Température en fonction de l'altitude en Kelvin
+def Temp(z):
+    def aux (z):
+        if z < 11e3 : return -6.5*z*1e-3 + 15
+        elif z < 20e3 : return -56.5
+        elif z < 32e3 : return 1*(z - 20e3)*1e-3 - 56.5
+        elif z < 47e3 : return 2.8*(z - 32e3)*1e-3 - 44.5
+        elif z < 51e3 : return -2.5
+        elif z < 71e3 : return -2.8*(z - 51e3)*1e-3 - 2.5
+        else : return -2*(z - 71e3)*1e-3 - 58.5
+    return aux(z) + 273.15
 
 ##Équation differentielle de la pression en fonction de l'altitude
 def P(Y,z):
 	M = 28.956 #g/mol
 	R = 8.314e3
-	return np.array([-Y[0] * M * g(z) / (R * (Temp(z) + 273.15))])
+	return np.array([-Y[0] * M * g(z) / (R * Temp(z))])
 
 ##Résolution de l'équation différentielle de la pression
 CI = np.array([p0])
 p = odeint(P, CI, z)
 
-#plt.figure()
-#plt.plot(z,p, color='red')
-#plt.ylabel('Pression en Pa')
-#plt.xlabel('Altitude en m')
-#plt.grid()
-#plt.show()
+# plt.figure()
+# plt.plot(z,p, color='red')
+# plt.ylabel('Pression en Pa')
+# plt.xlabel('Altitude en m')
+# plt.grid()
+# plt.show()
 
 ##Première phase du vol
 
@@ -62,9 +64,9 @@ def rho_air(z):
     y = round(z)
     M_air = 0.01801528 #! Check this data
     R = 8.314
-    return p[y][0] * M_air / (R * (Temp(z) + 273.15))
+    return p[y][0] * M_air / (R * Temp(z))
 
-def frottements(z, m, v):
+def frottements(z, v):
     r1 = 6.4 / 2
     r2 = 8.6 / 2
     S = np.pi * (r1**2 + r2**2)
@@ -88,21 +90,22 @@ def trajectoire ():
     
     v = 0
     
-    T = np.linspace(0,60,N) #tableau numpy du temps
+    T = np.linspace(0,400,N) #tableau numpy du temps
     
     for i in range (1,N) : 
     
         m, out_of_fuel = masse(T[i])
         p = propulsion(out_of_fuel)
         # X[i] = 1/2*g(Y[i-1])*np.sin(alpha)*T[i]**2
-        v = (p/m - g(Y[i-1]) - frottements(Y[i - 1], m, v) * np.sign(v)) * T[i]
+        print("Altitude // ", i, ": ", Y[i-1])
+        v = (p/m - g(Y[i-1]) - frottements(Y[i - 1], v) * np.sign(v) / m) * T[i]
         Y[i] = 1/2*v*T[i]
         
         
     return X,Y
     
-
-plt.plot(trajectoire()[0],trajectoire()[1])
+x, y = trajectoire()
+plt.plot(x,y)
 plt.xlabel ('x en m')
 plt.ylabel ('z en m')
 plt.show()
